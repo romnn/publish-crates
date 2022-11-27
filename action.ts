45c54040bfdb21959ff78da163a9c654ac47aebb
romnnn <roman.dahm@gmx.de>
2022-11-27 23:16:21 +0100

    add local action test

--- action.ts
+++ action.ts
@@ -1,15 +1,39 @@
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
+import {
+  parseCargoPackageManifest,
+  Repo,
+  RustTarget,
+} from "action-get-release";
 import * as path from "path";
-import {Repo, RustTarget} from "action-get-release";
 
 async function run(): Promise<void> {
-  const version: string = "v0.0.1";
+  let version = "latest";
+  let versionOverride = core.getInput("version");
+  if (versionOverride && versionOverride !== "") {
+    version = versionOverride;
+  } else {
+    try {
+      // read the version from cargo manifest
+      let manifest = await parseCargoPackageManifest("Cargo.toml");
+      version = manifest.package.version;
+    } catch (err: unknown) {
+      core.warning(`failed to read version from Cargo.toml: ${err}`);
+    }
+  }
+  core.debug(`version=${version}`);
+
   const repo = new Repo();
 
-  const release = (version === "" || version === "latest")
-                      ? await repo.getLatestRelease()
-                      : await repo.getReleaseByTag(version);
+  let release;
+  try {
+    release = version === "" || version === "latest"
+                  ? await repo.getLatestRelease()
+                  : await repo.getReleaseByTag(version);
+  } catch (err: unknown) {
+    throw new Error(
+        `failed to fetch ${version} release for ${repo.fullName()}: ${err}`);
+  }
   core.debug(`found ${release.assets().length} assets for ${
       version} release of ${repo.fullName()}`);
 
@@ -19,7 +43,13 @@ async function run(): Promise<void> {
   // publish-crates-action-x86_64-unknown-linux-gnu.tar.gz
   const asset = `publish-crates-action-${arch}-unknown-${platform}-gnu.tar.gz`;
 
-  const downloaded = await release.downloadAsset(asset, {cache : false});
+  let downloaded;
+  try {
+    downloaded = await release.downloadAsset(asset, {cache : false});
+  } catch (err: unknown) {
+    throw new Error(`failed to download asset ${asset}: ${err}`);
+  }
+
   // core.addPath(downloaded);
   const executable = path.join(downloaded, "publish-crates-action");
   await exec.exec(executable);
